f3d3f1b2e687b1cfff5fd16de033ebc52a30cba2
thiago alves <thiago87gsa@gmail.com>
2023-12-25 12:25:33 -0300

    firs commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from '@/styles/Navbar.module.css'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image
+          src="/images/pokeball.png"
+          width={30}
+          height={30}
+          alt="pokeball"
+        />
+        <h1>PokeNext</h1>
+      </div>
+      <ul className={styles.link_items}>
+        <li>
+          <Link className={styles.link} href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/about">
+            Sobre
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
